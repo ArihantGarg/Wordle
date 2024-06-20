cbae4fc1731b896a3f14f78c222bf91cc0f87118
Arihant Garg <arihantgarg.ishu@gmail.com>
2024-06-20 16:03:50 +0530

    Added word finder

---
+++ Game/findPossibleWord.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#include "../Headers/gameFunctions.hpp"
+#include "../Headers/colorFunctions.hpp"
+
+vector<string> orderedWords=getOrderedWords();
+vector<string> orderedAnswers=getOrderedAnswers();
+
+set<string> setWords(orderedAnswers.begin(),orderedAnswers.end());
+
+void updateSetWords(vector<char> containsChars)
+{
+    vector<string> toErase;
+
+    for(auto x:setWords)
+    {
+        for(auto y:containsChars)
+        {
+            if(x.find(y)==string::npos)
+            {
+                toErase.push_back(x);
+                break;
+            }
+        }
+    }
+
+    for(auto x:toErase)
+    {
+        setWords.erase(x);
+    }
+}
+
+int main(int argc,char* argv[])
+{
+  updateSetWords({'e','l','c'});
+
+  for(auto curWord:setWords)
+  {
+    cout<<curWord<<endl;
+  }
+
+  return 0;
+}
